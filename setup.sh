47f71496df208d1e749d60329e9e9568d1dded8f
Veronica <Vernonicuhhh@protonmail.com>
2023-08-20 20:01:40 -0400

    added basic setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+echo "Creating Newsboat Directory"
+mkdir ~/.config/newsboat/
+cp -r newsboat ~/.config/
+
+echo "Setting up .vimrc"
+cp vim/.vimrc ~/
+
+echo "Setting up .vimrc plug"
+cp vim/.vimrc.plug ~/
